3673ba936e6785b8cb4d1eed593f9bd72decc1a4
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-14 19:36:13 +0200

    Add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,18 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Menu from './Menu';
+import Usuarios from './Usuarios';
 
+const Prueba = () => <div>hola</div>
 
-class App extends Component {
-	constructor(){
-		super();
-		this.state = {
-			usuarios: []
-		}
-	}
-
-	async componentDidMount () {
-		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-		console.log('respuesta: ', respuesta);
-		this.setState({		
-			usuarios: [
-				{
-					nombre: 'Rodolfo',
-					correo: 'Rodolfo@platzi.com',
-					enlace: 'Rodolfo.com'
-				},
-				{
-					nombre: 'Platzi',
-					correo: 'Platzi@platzi.com',
-					enlace: 'Platzi.com'
-				}				
-			]}
-		);
-	}
-
-	ponerFilas = () => (
-		this.state.usuarios.map((usuario) => (
-			<tr>
-				<td>{ usuario.nombre }</td>
-				<td>{ usuario.correo }</td>
-				<td>{ usuario.enlace }</td>
-			</tr>
-		))
-	);
-
-	render (){
-		return (
-			<div className="margen">
-				<table className="tabla">
-					<thead>
-						<tr>
-							<th>
-								Nombre
-							</th>
-							<th>
-								Correo
-							</th>
-							<th>
-								Enlace
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{ this.ponerFilas() }
-					</tbody>
-				</table>
-			</div>
-		);
-	}
-};
+const App = (props) => (
+	<BrowserRouter>
+		<Menu />
+		<div id="margen">
+			<Route exact path='/' component={Usuarios} />
+			<Route exact path='/tareas' component={Prueba} />
+		</div>
+	</BrowserRouter>
+);
 
 export default App;
