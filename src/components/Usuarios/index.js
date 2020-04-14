271c59a4c85af293c640a5453e726115e05b6974
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-15 00:38:26 +0200

    Add Connection to a component

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Usuarios extends Component {
-	constructor() {
-		super();
-		this.state = {
-			usuarios: []
-		}
-	}
-
+/*
 	async componentDidMount() {
 		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
 		this.setState({
 			usuarios: respuesta.data
 		});
 	}
-
-	ponerFilas = () => this.state.usuarios.map((usuario) => (
+*/
+	ponerFilas = () => (
+		this.props.usuarios.map((usuario) => (
 		<tr key={ usuario.id }>
 			<td>
 				{ usuario.name }
@@ -28,7 +24,8 @@ class Usuarios extends Component {
 				{ usuario.website }
 			</td>
 		</tr>
-	));
+	))
+	);
 
 	render() {
 		return (
@@ -56,4 +53,8 @@ class Usuarios extends Component {
 	}
 };
 
-export default Usuarios;
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps, {})(Usuarios);
