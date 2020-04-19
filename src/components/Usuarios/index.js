3f3112813132b6fadd63b27d1a817f3f87b31268
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-19 17:55:26 +0200

    Add Redux Thunk

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 
 import * as usuariosActions from '../../actions/usuariosActions';
 
 class Usuarios extends Component {
+
 	componentDidMount() {
-		/*
-		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-		this.setState({
-			usuarios: respuesta.data
-		});
-		*/
 		this.props.traerTodos();
 	}
+
 	ponerFilas = () => (
 		this.props.usuarios.map((usuario) => (
 		<tr key={ usuario.id }>
@@ -31,7 +26,6 @@ class Usuarios extends Component {
 	);
 
 	render() {
-		console.log(this.props);
 		return (
 			<div>
 				<table className="tabla">
