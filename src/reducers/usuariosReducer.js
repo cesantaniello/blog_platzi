88a006f5bcb18a1f89fa4575ff729e06801b139d
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-19 23:50:42 +0200

    Add Type files

--- src/reducers/usuariosReducer.js
+++ src/reducers/usuariosReducer.js
@@ -1,12 +1,14 @@
+import { TRAER_TODOS } from '../types/usuariosTypes'; 
+
 const INITIAL_STATE = {
   usuarios: []
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type){
-    case 'traer_usuarios':
+    case TRAER_TODOS:
       return { ...state, usuarios: action.payload}
 
-      default: return state;
+    default: return state;
   }
 }
