e029ca805045bddf169206c3984d0ecb09fb80f7
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-22 22:19:27 +0200

    Add componentDidMount to Publicaciones class

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as usariosActions from '../../actions/usuariosActions';
 
 class Publicaciones extends Component {
+	componentDidMount() {
+		if (!this.props.usuarios.length) {
+			this.props.traerTodos();
+		}
+	}
+
 	render() {
+		console.log(this.props);
 		return (
 			<div>
+				<h1>Publicaciones de </h1>
 				{ this.props.match.params.key }
 			</div>
 		);
 	}
 }
 
-export default Publicaciones;
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps, usariosActions)(Publicaciones);
