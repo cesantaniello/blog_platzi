101a5b3e614c08409d7621b0e59dc0b155051eb7
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-04-27 16:08:39 +0200

    Component to add tasks

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Spinner from '../General/Spinner';
 import Fatal from '../General/Fatal';
 
@@ -46,9 +47,13 @@ class Tareas extends Component {
 	};
 	
 	render() {
-		console.log(this.props);
 		return (
 			<div>
+				<button>
+					<Link to='/tareas/guardar'>
+						Agregar
+					</Link>
+				</button>
 				{ this.mostrarContenido()}
 			</div>
 		);
